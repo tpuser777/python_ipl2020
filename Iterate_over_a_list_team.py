bd3cf6a4bd29bea1d18d1c86f07d67cb39835214
tpuser777 <63839869+tpuser777@users.noreply.github.com>
2020-10-12 15:30:20 +0530

    Create Iterate_over_a_list_team.py

---
+++ Iterate_over_a_list_team.py
@@ -0,0 +1,7 @@
+team_all = ["Mumbai Indians", "Chennai Super Kings" , "Delhi Capitals", "Kings XI Punjab" , "Kolkata Knight Riders", "Rajasthan Royals" , "Royal Challengers Bangalore", 
+"Sunrisers Hyderabad" ]
+
+for i in team_all:
+ print (i)
+ 
+ 
