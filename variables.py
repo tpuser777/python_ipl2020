4c2ac9fdec0f88c751bfdb033f8626b261685748
tpuser777 <63839869+tpuser777@users.noreply.github.com>
2020-10-12 15:21:02 +0530

    Update variables.py

--- variables.py
+++ variables.py
@@ -6,3 +6,12 @@ team_five = "Kolkata Knight Riders"
 team_six = "Rajasthan Royals"
 team_seven = "Royal Challengers Bangalore"
 team_eight = "Sunrisers Hyderabad"
+
+print (team_one)
+print (team_two)
+print (team_three)
+print (team_four)
+print (team_five)
+print (team_six)
+print (team_seven)
+print (team_eight)
