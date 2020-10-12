ee06f78ff5d1ed7fe4d2354a1636befd76bd6b39
tpuser777 <63839869+tpuser777@users.noreply.github.com>
2020-10-12 18:56:15 +0530

    Create compare_wickets.py

---
+++ compare_wickets.py
@@ -0,0 +1,13 @@
+if True:
+  print("Hello Python If")
+
+if 2 > 1:
+  print("2 is greater than 1")
+  
+wickets_taken_by_Jasprit_Bumrah = 11
+wickets_taken_by_Rashid_Khan = 10
+  
+if wickets_taken_by_Jasprit_Bumrah > wickets_taken_by_Rashid_Khan:
+  print("Jasprit is the best bowler")
+else:
+  print("Rashid is the best bowler")
