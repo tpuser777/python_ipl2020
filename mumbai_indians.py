c1c8fa7a57322e53c6c5c713af1d63bd47500c6a
tpuser777 <63839869+tpuser777@users.noreply.github.com>
2020-10-12 16:17:23 +0530

    Update mumbai_indians.py

--- mumbai_indians.py
+++ mumbai_indians.py
@@ -1,15 +1,21 @@
 # make a dictionary
 
 team_composition = {
-  "keeper": "Quinton de Kock",
-  "Batsman1": "Rohit Sharma",
-  "Batsman1": "Suryakumar Yadav",
-  "Batsman1": "Ishan Kishan",
-  "Batsman1": "Hardik Pandya",
-  "Batsman1": "Kieron POLLARD",
-  "Batsman1": "Krunal Pandya",
-  "Batsman1": "James Pattinson",
-  "Batsman1": "Trent Boult",
-  "Batsman1": "Jasprit Bumrah",
-  "Bowler" : "Rahul Chahar"
+  "Keeper" : "Quinton de Kock",
+  "Batsman 1" : "Rohit Sharma",
+  "Batsman 2" : "Suryakumar Yadav",
+  "Batsman 3" : "Ishan Kishan",
+  "Batsman 4" : "Hardik Pandya",
+  "All Rounder 1" : "Kieron POLLARD",
+  "All Rounder 2" : "Krunal Pandya",
+  "All Rounder 3" : "Rahul Chahar",
+  "Bowler 1" : "James Pattinson",
+  "Bowler 2" : "Trent Boult",
+  "Bowler 3" : "Jasprit Bumrah",
 }
+
+print (team_composition["Keeper"])
+
+print (team_composition["Bowler 1"])
+
+print (team_composition["Batsman 1"])
