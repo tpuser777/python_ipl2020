4e9334d2ab1875997e0bd3a79b25bf10debef3c8
tpuser777 <63839869+tpuser777@users.noreply.github.com>
2020-10-12 15:51:50 +0530

    Update mumbai_indians.py

--- mumbai_indians.py
+++ mumbai_indians.py
@@ -1,2 +1,15 @@
 # make a dictionary
 
+team_composition = {
+  "keeper": "Quinton de Kock",
+  "Batsman1": "Rohit Sharma",
+  "Batsman1": "Suryakumar Yadav",
+  "Batsman1": "Ishan Kishan",
+  "Batsman1": "Hardik Pandya",
+  "Batsman1": "Kieron POLLARD",
+  "Batsman1": "Krunal Pandya",
+  "Batsman1": "James Pattinson",
+  "Batsman1": "Trent Boult",
+  "Batsman1": "Jasprit Bumrah",
+  "Bowler" : "Rahul Chahar"
+}
