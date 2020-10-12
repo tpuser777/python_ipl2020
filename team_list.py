b4fcec4af607b5a926ede65b810d27f496b8f7e9
tpuser777 <63839869+tpuser777@users.noreply.github.com>
2020-10-12 15:24:04 +0530

    Create team_list.py

---
+++ team_list.py
@@ -0,0 +1,4 @@
+team_all = ["Mumbai Indians", "Chennai Super Kings" , "Delhi Capitals", "Kings XI Punjab" , "Kolkata Knight Riders", "Rajasthan Royals" , "Royal Challengers Bangalore",
+"Sunrisers Hyderabad" ]
+
+print (team_all)
