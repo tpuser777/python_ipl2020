0d9b5dae74b8dff3f0b9bace0f7068ad7ae5eaab
tpuser777 <63839869+tpuser777@users.noreply.github.com>
2020-10-12 15:44:35 +0530

    Update team_list.py

--- team_list.py
+++ team_list.py
@@ -7,11 +7,11 @@ print(team_all[0])
 print(team_all[1]) 
 
 # Create a new team
-team_all_2021 = []
+team_all_2021 = team_all
 
-team_all_2021 = team_all.append("Assam Tigers")
+team_all_2021.append("Assam Tigers")
 print (team_all_2021)
-team_all_2021 = team_all.append("Ranchi Riders")
+team_all_2021.append("Ranchi Riders")
 print (team_all_2021)
 
 print (team_all_2021)
